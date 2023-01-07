37cab2df32e20dab39c4884608e3be3f3f97c141
Nightburnn <nightburn2003@gmail.com>
2023-01-07 09:38:17 -0800

    emo-tion

---
+++ emoji.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+print("hello and welcome to emoji")
+
+print()
+
+
+
+stop = "no"
+
+
+
+while stop == "no":
+
+  emoji = input("what emotion are you feeling?: ")
+
+  if emoji == "happy" or emoji == "joy":
+
+    print("😊")
+
+  elif emoji == "sad" or emoji == "Sad":
+
+    print("😔")
+
+  elif emoji == "scared" or emoji == "fear":
+
+    print("😱")
+
+  elif emoji == "suprised" or emoji == "Suprised":
+
+    print("😲")
+
+  elif emoji == "disgust" or emoji == "Disgust":
+
+    print("🤢 🤮")
+
+  elif emoji == "anger" or emoji == "angry":
+
+    print("😡")
+
+  else:
+
+    print("I can't understand this emotion...please try again later")
+
